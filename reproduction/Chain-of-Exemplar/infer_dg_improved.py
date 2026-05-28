e1e90c6b042e50c93d195d6ef395311e26b882af
Kajal A <kawasthi@scu.edu>
2026-05-27 18:44:14 -0700

    Add local RG/DG reproduction and DG prompt evaluation

---
+++ reproduction/Chain-of-Exemplar/infer_dg_improved.py
@@ -0,0 +1,43 @@
+import json
+import os
+import torch
+from tqdm import tqdm
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
+
+MODEL_PATH = "output/dg_lora_local"
+DATA_PATH = "data/ScienceQA_validation_dg_improved.json"
+OUT_PATH = "infer/pred_validation_dg_improved_dgmodel.json"
+
+os.makedirs("infer", exist_ok=True)
+
+data = json.load(open(DATA_PATH))
+
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_PATH,
+    trust_remote_code=True
+)
+
+model = AutoPeftModelForCausalLM.from_pretrained(
+    MODEL_PATH,
+    device_map="auto",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True
+).eval()
+
+preds = []
+
+for ex in tqdm(data):
+    prompt = ex["conversations"][0]["value"]
+    response, _ = model.chat(tokenizer, query=prompt, history=None, do_sample=False)
+
+    preds.append({
+        "id": ex["id"],
+        "response": response
+    })
+
+json.dump(preds, open(OUT_PATH, "w"), indent=2)
+
+print("saved", OUT_PATH)
+print("samples", len(preds))
+print("first prediction:", preds[0])
